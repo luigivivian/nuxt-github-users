8e76394baa51a6932367aeccd344a430cba88015
luigivivian <luigivivian@hotmail.com>
2022-01-12 16:22:15 -0300

    fix middleware

--- middleware/load-user-and-repos.js
+++ middleware/load-user-and-repos.js
@@ -1,11 +1,7 @@
 
-export default async function({store, from, params}){
+export default async function({store, params}){
     // middleware para carregamento de dados
-    const isInitialState = !from;
-    if(isInitialState){
-        await store.dispatch('users/getUserByName', params.name);
-        await store.dispatch('users/getRepos', params.name);
-        await store.dispatch('users/getStarredRepos', params.name);
-
-    }
+    await store.dispatch('users/getUserByName', params.name);
+    await store.dispatch('users/getRepos', params.name);
+    await store.dispatch('users/getStarredRepos', params.name);
 }
